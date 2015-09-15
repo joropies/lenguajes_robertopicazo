902f2482c2f951ef2afd3aa987b7ea44f9203233
T-107 <T-107@PC251>
2015-09-14 19:27:24 -0500

    Profesor, Ya está el primer ejercicio de la clase

---
+++ lenguajes_robertopicazo/src/lenguajes_robertopicazo/Lenguajes_robertopicazo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lenguajes_robertopicazo;
+
+/**
+ *
+ * @author T-107
+ */
+public class Lenguajes_robertopicazo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Mi primer programa de Java");
+    }
+    
+}
