4e7f5680a193de7a0b769639bb15d21c25d5ff70
T-107 <T-107@PC250>
2015-10-01 19:47:11 -0500

    Diseño de ventana Administración de Clientes / Base de Datos

---
+++ lenguajes_robertopicazo/src/model/Direccion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author T-107
+ */
+public class Direccion {
+    
+    String calle;
+    String colonia;
+    String municipio;
+    
+}
