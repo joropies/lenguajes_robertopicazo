8b0baf8846892dc73cdf0f1154bd465e74f930e2
T-107 <T-107@PC250>
2015-11-23 19:48:58 -0600

    graficadora parabola

--- lenguajes_robertopicazo/src/ProyectoFinal/ContenedorGraficadora.java
+++ lenguajes_robertopicazo/src/ProyectoFinal/ContenedorGraficadora.java
@@ -7,6 +7,7 @@ package ProyectoFinal;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Event;
 import java.awt.Graphics;
 
 /**
@@ -14,6 +15,7 @@ import java.awt.Graphics;
  * @author T-107
  */
 public class ContenedorGraficadora extends Canvas{
+    public static float apertura = 0.01f;
 
     @Override
     public void paint(Graphics g) {
@@ -21,14 +23,40 @@ public class ContenedorGraficadora extends Canvas{
         // super = invoca la version del papa para usarlo
         
         g.setColor(Color.white);
-        g.fillRect(0, 0, getHeight(), getHeight());
+        g.fillRect(0, 0, getWidth(), getHeight());
         // getwidth da el total de la pantalla/lugar donde se va a expandir
         g.setColor(Color.black);
         // con esta definicio del color quedaria negro
         g.drawLine(0, getHeight()/2, getWidth(), getHeight()/2);
         g.drawLine(getWidth()/2, 0, getWidth()/2, getHeight());
+        g.translate(getWidth()/2, getHeight()/2);
+        
+        g.drawString("Origen de Coordenadas", 0, 0);
+        
+        // grafiquemos una parabola
+        
+        float x0 = -100;
+        float x1 = -99;
+        float y0;
+        float y1;
+        
+        for (int i=0; i <= 200; i++){
+        /*
+            y0 = -apertura *MiGraficadora.a* x0 * x0 +MiGraficadora.b*x0+MiGraficadora.c;
+            y1 = -apertura *MiGraficadora.a* x1 * x1 +MiGraficadora.b*x1+MiGraficadora.c;
+        */
+            
+            y0 = -apertura * x0 * x0 - 10;
+            y1 = -apertura * x1 * x1 - 10;
+            
+            g.drawLine((int)x0, (int)y0, (int)x1, (int)y1);
+            x0++;
+            x1++;
+        
+        }
         
         // funcion seno hay que revisarla porque no esta************
+        /*
         double xseno0 = -2 * Math.PI - 1;
         double xseno1 = -2 * Math.PI;
         double yseno0;
@@ -43,11 +71,22 @@ public class ContenedorGraficadora extends Canvas{
         
         }
         //*******************************************************************
-        
+    */    
     }
     // la clase canvas es para graficos
     // tiene un metodo que se llama paint
+
     
+    // este metodo esta 
+    @Override
+    public boolean mouseDown(Event evt, int x, int y) {
+        System.out.println("Has hecho clic sobre la gráfica");
+        System.out.println("Posicion es: " + "(" + x + "," + y + ")");
+        
+        return super.mouseDown(evt, x, y); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    //
     
 
 }
--- lenguajes_robertopicazo/src/ProyectoFinal/MiGraficadora.java
+++ lenguajes_robertopicazo/src/ProyectoFinal/MiGraficadora.java
@@ -10,6 +10,11 @@ package ProyectoFinal;
  * @author T-107
  */
 public class MiGraficadora extends javax.swing.JFrame {
+    // se pone como atributo porque se va a acceder al factor de escala
+    ContenedorGraficadora contenedor;
+    public static float a;
+    public static float b;
+    public static float c;
 
     /**
      * Creates new form MiGraficadora
@@ -33,6 +38,15 @@ public class MiGraficadora extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        boton = new javax.swing.JButton();
+        textoAmplitud = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        textoA = new javax.swing.JTextField();
+        textoB = new javax.swing.JTextField();
+        textoC = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        textoD = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,28 +55,127 @@ public class MiGraficadora extends javax.swing.JFrame {
 
         jPanel1.setLayout(new java.awt.BorderLayout());
 
+        jLabel2.setText("Factor de Amplitud");
+
+        boton.setText("Graficar");
+        boton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonActionPerformed(evt);
+            }
+        });
+
+        textoAmplitud.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textoAmplitudActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Resolver la ecuacion cuadratica encontrando sus raices");
+
+        textoB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textoBActionPerformed(evt);
+            }
+        });
+
+        textoC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textoCActionPerformed(evt);
+            }
+        });
+
+        jLabel7.setText("ax2      +    bx     +     cy      +     d     =   0");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(37, 37, 37)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(textoAmplitud, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(9, 9, 9)
+                                .addComponent(boton))
+                            .addComponent(jLabel3)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel7)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(textoA, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(textoB, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(textoC, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(textoD, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1))
-                .addContainerGap(54, Short.MAX_VALUE))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(74, 74, 74))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(29, 29, 29)
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(38, 38, 38)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(textoAmplitud, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(boton)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)
+                        .addGap(49, 49, 49)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(textoA, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(textoB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(textoC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(textoD, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(12, 12, 12)
+                        .addComponent(jLabel7)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void botonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonActionPerformed
+        // TODO add your handling code here:
+        
+        float valor = Float.parseFloat(textoAmplitud.getText());
+        float abrir = 1 / ((valor) * 100);
+        /*
+        a=Float.parseFloat(textoA.getText());
+        b=Float.parseFloat(textoB.getText());
+        c=Float.parseFloat(textoC.getText());
+        */
+        ContenedorGraficadora.apertura = abrir;
+        contenedor.repaint();
+    }//GEN-LAST:event_botonActionPerformed
+
+    private void textoBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textoBActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textoBActionPerformed
+
+    private void textoCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textoCActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textoCActionPerformed
+
+    private void textoAmplitudActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textoAmplitudActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textoAmplitudActionPerformed
 
     /**
      * @param args the command line arguments
@@ -100,7 +213,16 @@ public class MiGraficadora extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton boton;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField textoA;
+    private javax.swing.JTextField textoAmplitud;
+    private javax.swing.JTextField textoB;
+    private javax.swing.JTextField textoC;
+    private javax.swing.JTextField textoD;
     // End of variables declaration//GEN-END:variables
 }
